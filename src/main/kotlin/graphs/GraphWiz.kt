545e1af5b04de25dfe58f45f2cc1a9e8e414d5b0
Prajith Nair <kprajith@amazon.com>
2018-12-09 17:04:29 +0000

    https://www.hackerrank.com/challenges/the-story-of-a-tree/submissions/code/92565672
    ALso adding support for graphwize

---
+++ src/main/kotlin/graphs/GraphWiz.kt
@@ -0,0 +1,57 @@
+package main.kotlin.graphs
+
+import java.io.File
+
+class GraphWiz(private val root: Node, val path: String = ".") {
+
+    companion object {
+        val FILE_NAME = "graphwiz.dot"
+    }
+
+    private val nodes = mutableMapOf<Int, Node>()
+
+    private val file = File("$path/$FILE_NAME")
+
+    init {
+        nodes.put(root.id, root)
+        file.writeText("")
+    }
+
+    data class Node(val id: Int, val children: MutableSet<Int> = mutableSetOf())
+
+
+    fun add(node: Node, asAChildOf: Int) {
+        val parent = nodes.getOrDefault(asAChildOf, Node(asAChildOf))
+        val child = nodes.getOrDefault(node.id, node)
+        parent.children.add(node.id)
+        nodes[asAChildOf] = parent
+        nodes[node.id] = child
+    }
+
+    fun start() {
+        nodes.clear()
+        nodes.put(root.id, root)
+        file.appendText("@startuml\n")
+    }
+
+    fun stop() {
+        file.appendText("@enduml\n")
+    }
+
+    fun appendToDotFile() {
+        file.appendText("graph G {\n")
+        file.appendText(bfs(root))
+        file.appendText("}\n")
+        nodes.clear()
+        nodes.put(root.id, root)
+    }
+
+    private fun bfs(root: Node): String {
+        var output = ""
+        root.children.forEach {
+            output += "\"${root.id}\" -- \"$it\"\n"
+            output += bfs(nodes[it]!!)
+        }
+        return output
+    }
+}
--- src/main/kotlin/hackerrank/Solution.kt
+++ src/main/kotlin/hackerrank/Solution.kt
@@ -2,14 +2,17 @@ package main.kotlin.hackerrank
 
 import java.io.File
 import java.util.*
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
 import kotlin.system.measureTimeMillis
 
 /******* utility functions *************/
 
+data class DevOverrides(var isDebug: Boolean, var readFromFile: Boolean)
 
-data class Ref<T>(var value: T)
 
-val isDevelopment = Ref(false)
+val devOverrides = DevOverrides(false, false)
 
 inline fun <T : Any> T?.whenNotNull(f: (it: T) -> Unit) {
     if (this != null) f(this)
@@ -20,13 +23,14 @@ inline fun <T : Any> T?.whenNull(f: () -> Unit) {
 }
 
 enum class Level {
+    ERROR,
     DEBUG,
     ACTUAL
 }
 
 fun log(message: Any?, level: Level) {
     when (level) {
-        Level.DEBUG -> if (isDevelopment.value) println("DEBUG: $message")
+        Level.DEBUG, Level.ERROR -> if (devOverrides.isDebug) println("$level: $message")
         Level.ACTUAL -> println(message)
     }
 }
@@ -35,118 +39,217 @@ fun log(message: Any?) {
     println(message)
 }
 
-fun debugLog(message: Any?) {
-    log(message, Level.DEBUG)
+fun debugLog(message: Any) {
+    debugLog(Level.DEBUG) { message.toString() }
 }
 
-inline fun <T> withTimeToExecution(operationName: String = "Default", block: () -> T): T {
+fun debugLog(level: Level = Level.DEBUG, message: Any) {
+    debugLog(level) { message.toString() }
+}
+
+inline fun debugLog(level: Level = Level.DEBUG, block: () -> Any) {
+    if (devOverrides.isDebug) {
+        log(block(), level)
+    }
+
+}
+
+inline fun <T> withTimeToExecution(operationName: String = "Overall", block: () -> T): T {
     var value: T? = null
     val time = measureTimeMillis {
         value = block()
     }
-    debugLog("Elapsed time: $time ms for Operation: $operationName")
+    debugLog { "Elapsed time: $time ms for Operation: $operationName" }
     return value!!
 }
 
+fun <T> completeWithin(timeoutInMilliSecs: Long = 5000, block: () -> T): T? {
+    try {
+        return CompletableFuture
+            .supplyAsync(block)
+            .get(timeoutInMilliSecs, TimeUnit.MILLISECONDS)
+    } catch (e: TimeoutException) {
+        debugLog(Level.ERROR, "Failed to complete within $timeoutInMilliSecs ms")
+        return null
+    }
+}
 
 fun setDevelopmentFlag(args: Array<String>) {
-    isDevelopment.value = args.contains("test")
+    devOverrides.isDebug = args.contains("test")
+    devOverrides.readFromFile = args.contains("readFromFile")
 }
+
+fun scanner(): Scanner {
+    if (devOverrides.readFromFile) {
+        return Scanner(File("/Users/kprajith/Desktop/REMOVE_THIS.txt"))
+    } else {
+        return Scanner(System.`in`)
+    }
+}
+
 
 /******* utility functions *************/
 
-enum class Type {
-    StartCloud,
-    EndCloud,
-    Town
-}
+data class Node(
+    val id: Int,
+    val children: MutableList<Int> = mutableListOf(),
+    var towards: Int = 0,
+    var away: Int = 0
+)
 
-data class Position(val coordinate: Long, val type: Type, val value: Long, val id: Int)
+//var graphWiz: GraphWiz = GraphWiz(GraphWiz.Node(1))
+
 
 fun main(args: Array<String>) {
     setDevelopmentFlag(args)
-    //90394853496857 - expected
-    val scan = Scanner(File("/Users/kprajith/Desktop/REMOVE_THIS.txt"))
-//    val scan = Scanner(System.`in`)
-    val n = scan.nextLine().trim().toInt()
-    val p = scan.nextLine().split(" ").map { it.trim().toLong() }.toTypedArray()
-    val x = scan.nextLine().split(" ").map { it.trim().toLong() }.toTypedArray()
-    val m = scan.nextLine().trim().toInt()
-    val y = scan.nextLine().split(" ").map { it.trim().toLong() }.toTypedArray()
-    val r = scan.nextLine().split(" ").map { it.trim().toLong() }.toTypedArray()
-    val result = withTimeToExecution { maximumPeople(p, x, y, r, n, m) }
-    println(result)
+    val scan = scanner()
+    completeWithin(5000) {
+        withTimeToExecution {
+            //            graphWiz.start()
+            val q = scan.nextLine().trim().toInt()
+            for (qItr in 1..q) {
+                val n = scan.nextLine().trim().toInt()
+                val edges = Array(n - 1) { Array(2) { 0 } }
+                for (edgesRowItr in 0 until n - 1) {
+                    edges[edgesRowItr] = scan.nextLine().split(" ").map { it.trim().toInt() }.toTypedArray()
+                }
+                val gk = scan.nextLine().split(" ")
+                val g = gk[0].trim().toInt()
+                val k = gk[1].trim().toInt()
+                val guesses = Array(g) { Array(2) { 0 } }
+                for (guessesRowItr in 0 until g) {
+                    guesses[guessesRowItr] = scan.nextLine().split(" ").map { it.trim().toInt() }.toTypedArray()
+                }
+                val result = storyOfATree(n, edges, k, guesses)
+                println(result)
+            }
+//            graphWiz.stop()
+        }
+
+    }
 }
 
-fun maximumPeople(
-    people: Array<Long>,
-    towns: Array<Long>,
-    clouds: Array<Long>,
-    ranges: Array<Long>,
+fun storyOfATree(
     n: Int,
-    m: Int
-): Long {
-    val positions = getSortedPositions(n, towns, people, m, clouds, ranges)
-    var peopleInASunnyTown = 0L
-    val potentialSingleCloudCandidates = mutableListOf<Pair<Int, Long>>()
-    var cloudsAboveTown = mutableSetOf<Int>()
-    val positionMap = mutableMapOf<Long, Long>()
-    positions.forEach { (position, type, value, id) ->
-        //        debugLog("Position(pos: $position, type: $type, value: $value, id: $id) - overlapping count: ${cloudsAboveTown.size}" )
-        when (type) {
-            Type.StartCloud -> {
-                positionMap.merge(position, 1, Long::plus)
-                cloudsAboveTown.add(id)
-            }
-            Type.EndCloud -> {
-                positionMap.merge(position, 1, Long::plus)
-                cloudsAboveTown.remove(id)
-            }
-            Type.Town -> {
-                val count = positionMap.getOrDefault(position, 0)
+    edges: Array<Array<Int>>,
+    k: Int,
+    guesses: Array<Array<Int>>
+): String {
+    val visited = mutableMapOf<Int, Boolean>()
+    val nodes = mutableMapOf<Int, Node>()
+    populateNodes(edges, nodes)
+    traverseAndPopulateRoot(nodes, visited, guesses)
+    val possibleCount = nodes.values.filter { it.towards >= k }.count()
+    return asFraction(possibleCount, n)
+}
 
-                when {
-                    //Sunny
-                    cloudsAboveTown.isEmpty() && count == 0L -> peopleInASunnyTown += value
-                    //Just one cloud
-                    cloudsAboveTown.size == 1 && count == 0L -> potentialSingleCloudCandidates.add(
-                        Pair(
-                            cloudsAboveTown.first(),
-                            value
-                        )
-                    )
-                    //Ending of a cloud
-                    count <= 1 && cloudsAboveTown.isEmpty() -> potentialSingleCloudCandidates.add(Pair(id, value))
-                }
+fun asFraction(numerator: Int, denominator: Int): String {
+    if (numerator == denominator)
+        return "1/1"
+    if (numerator == 0)
+        return "0/1"
+    val gcd = gcd(numerator, denominator)
+    return String.format("%d/%d", numerator / gcd, denominator / gcd)
+}
+
+fun gcd(a: Int, b: Int): Int {
+    return if (b == 0) a else gcd(b, a % b)
+}
+
+fun traverseAndPopulateRoot(
+    nodes: MutableMap<Int, Node>,
+    visited: MutableMap<Int, Boolean>,
+    guesses: Array<Array<Int>>
+) {
+    val guessesSet = guesses.map { (parent, child) -> key(parent, child) }.toSet()
+//    debugLog {"Before \n" + nodes.values.joinToString("\n")}
+    bfsPopulateRoot(1, nodes, visited, guessesSet)
+//    graphWiz.appendToDotFile()
+//    debugLog { guessesSet.toString() }
+//    debugLog {"After bfsPopulateRoot  \n" + nodes.values.joinToString("\n")}
+    visited.clear()
+    bfsPopulateAllOtherNodes(1, nodes, visited, guessesSet)
+//    debugLog { guessesSet.toString() }
+//    debugLog {"After bfsPopulateAllOtherNodes \n" + nodes.values.joinToString("\n")}
+}
+
+private fun key(parent: Int, child: Int) = "$parent <- $child"
+
+
+fun bfsPopulateAllOtherNodes(
+    parent: Int,
+    nodes: MutableMap<Int, Node>,
+    visited: MutableMap<Int, Boolean>,
+    guesses: Set<String>
+) {
+    visited[parent] = true
+    val parentNode = nodes[parent]!!
+    parentNode.children
+        .filter { !visited.getOrDefault(it, false) }
+        .map { nodes[it]!! }
+        .forEach { child ->
+            child.towards += (parentNode.towards - child.towards)
+            child.away += (parentNode.away - child.away)
+            if (guesses.contains(key(parent, child.id))) { // parent <- child
+                child.towards--
+                child.away++
+            }
+            if (guesses.contains(key(child.id, parent))) { // child <- parent
+                child.away--
+                child.towards++
+            }
+            bfsPopulateAllOtherNodes(child.id, nodes, visited, guesses)
+
+        }
+}
+
+fun bfsPopulateRoot(
+    parent: Int,
+    nodes: MutableMap<Int, Node>,
+    visited: MutableMap<Int, Boolean>,
+    guesses: Set<String>
+): Pair<Int, Int> {
+    visited[parent] = true
+    val parentNode = nodes[parent]!!
+    parentNode.children
+        .filter { !visited.getOrDefault(it, false) }
+        .map { nodes[it]!! }
+        .forEach { child ->
+            //            graphWiz.add(GraphWiz.Node(child.id), parent)
+            if (guesses.contains(key(parent, child.id))) { // parent <- child
+                parentNode.towards++
+                child.away++
+            }
+            if (guesses.contains(key(child.id, parent))) { // child <- parent
+                parentNode.away++
+                child.towards++
+            }
+            val (awayFromSingleChild, towardsFromSingleChild) = bfsPopulateRoot(child.id, nodes, visited, guesses)
+            parentNode.away += awayFromSingleChild
+            parentNode.towards += towardsFromSingleChild
+
+            if (guesses.contains(key(parent, child.id))) { // remove the duplicate
+                parentNode.away--
+            }
+            if (guesses.contains(key(child.id, parent))) { // remove the duplicate
+                parentNode.towards--
             }
         }
-    }
-//    debugLog("peopleInASunnyTown: $peopleInASunnyTown")
-//    debugLog("potential single candidates: $potentialSingleCloudCandidates")
-    val singleCloudGrouping = potentialSingleCloudCandidates.groupBy({ it.first }, { it.second }).map { it.value.sum() }
-//    debugLog("singleCloudGrouping: $singleCloudGrouping")
-    val maxContribution = singleCloudGrouping.max() ?: 0L
-    return maxContribution + peopleInASunnyTown
-
+    return Pair(parentNode.away, parentNode.towards)
 }
 
-private fun getSortedPositions(
-    n: Int,
-    towns: Array<Long>,
-    people: Array<Long>,
-    m: Int,
-    clouds: Array<Long>,
-    ranges: Array<Long>
-): List<Position> {
-    val positions = mutableListOf<Position>()
-    for (i in 0 until n) {
-        positions.add(Position(towns[i], Type.Town, people[i], i))
+private fun populateNodes(
+    edges: Array<Array<Int>>,
+    nodes: MutableMap<Int, Node>
+) {
+    edges.forEach { (from, to) ->
+        val fromNode = nodes.getOrDefault(from, Node(from))
+        val toNode = nodes.getOrDefault(to, Node(to))
+        fromNode.children.add(to)
+        toNode.children.add(from)
+        nodes[from] = fromNode
+        nodes[to] = toNode
     }
-    for (i in 0 until m) {
-        positions.add(Position(clouds[i] - ranges[i], Type.StartCloud, 0, i))
-        positions.add(Position(clouds[i] + ranges[i], Type.EndCloud, 0, i))
-    }
-    return positions.sortedWith(compareBy({ it.coordinate }, { it.type }))
 }
 
 
